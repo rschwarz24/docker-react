b139a2d43b55895ae851f6eeada2c16a6620e50e
rschwarz24 <rschwarz24@t-online.de>
2020-10-04 11:25:39 +0200

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+describe('tests',()=>{
+  it('renders learn react link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
+  
+  it('renders learn react link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  }); 
+
+})
+
+
